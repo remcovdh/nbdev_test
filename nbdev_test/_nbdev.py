437024c04424d0eb8e60f8131f7ff61a0c9e32ee
REM & co <rem@test.com>
2019-12-20 21:59:14 +0000

    Just added one of the moved (or from gits perspective deleted) files

--- nbdev_test/_nbdev.py
+++ nbdev_test/_nbdev.py
@@ -2,7 +2,8 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"helloworld": "00_core.ipynb"}
+index = {"helloworld": "00_core.ipynb",
+         "helloworld2": "00_core.ipynb"}
 
 modules = ["core.py"]
 
